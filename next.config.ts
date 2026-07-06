95c4efd383fc10b9b47be7172f94702596ec5221
torogawi <torogawi@gmail.com>
2026-07-06 10:48:19 +0100

    Fix sftp vercel build error in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["ssh2-sftp-client", "ssh2"]
 };
 
 export default nextConfig;
